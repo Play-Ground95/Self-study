6242b2d341ffa2a7523ecdec2bb3c5097e98e8c7
Kavindu95 <kavindueng95@gmail.com>
2023-08-12 09:30:24 +0530

    1 to 10 numbers print

---
+++ Demo1.java
@@ -0,0 +1,10 @@
+public class Demo1{
+    public static void main(String[] args) {
+        int x=1;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("number is :"+x);
+            x++;
+            
+        }
+    }
+}
