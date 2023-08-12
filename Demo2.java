d0ef4bb12d04d5312149168f9451d330c5d3675b
Kavindu95 <kavindueng95@gmail.com>
2023-08-12 09:48:52 +0530

    1 to 10 nums SUM

---
+++ Demo2.java
@@ -0,0 +1,27 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        int x=0;
+        int y=0;
+        
+        for (int i = 0; i < 10; i++) {
+             y+=x+1;
+             x++;
+        }
+        System.out.println(y);
+    }
+
+    /*
+     public class SumNumbers
+{
+    public static void main(String[] args)
+    {
+        int sum = 0;
+        for(int i=1; i<=10; i++)
+        {
+            sum += i;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}   Answer from the website my answer is also correct
+     */
+}
